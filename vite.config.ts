d5d8c9808e231161d7ef7c98c89576691c10309d
nikita111bessonov111 <91000885+nikita111bessonov111@users.noreply.github.com>
2025-06-13 12:50:51 +0300

    Add GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: '/check_love_bot/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
