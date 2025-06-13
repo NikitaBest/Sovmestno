839757377925bae05231ff77d5e72b594c6a5819
nikita111bessonov111 <91000885+nikita111bessonov111@users.noreply.github.com>
2025-06-13 11:39:52 +0300

    fix: configs and aliases for GitHub Pages deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,1 +1,14 @@
- 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  base: "/Sovmestno/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "../shared"),
+    },
+  },
+}); 
